fe5e57e385ea1a67371684f1b39739e141e9ed8c
Sjaan Hydrick <sjaanahydrick@gmail.com>
2021-02-02 20:42:49 -0800

    back button added and styled

--- src/components/app/CatDetail.jsx
+++ src/components/app/CatDetail.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { getCat } from '../../services/catApi';
-import { useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 import styles from './App.css';
 
 const CatDetail = () => {
@@ -18,13 +18,14 @@ const CatDetail = () => {
     <>
       {chosenCat ?
         <div>
+          <Link to="/"><p className={styles.back}>&larr; Back to Home</p></Link>
           {
             chosenCat.reference_image_id === undefined ?
               <p className={styles.noImagePage}>No Image Found</p>
               : 
               <img src={`https://cdn2.thecatapi.com/images/${chosenCat.reference_image_id}.jpg`} className={styles.pagePic}/>
           }
-          <h3 className={styles.catName}>{chosenCat.name}</h3>
+          <h3 className={styles.catNamePage}>{chosenCat.name}</h3>
           <h4 className={styles.origin}>{chosenCat.origin}</h4>
           <p className={styles.desc}>{chosenCat.description}</p>
         </div>
